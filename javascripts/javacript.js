1581beb098b70bb2ea8eea4b0c4f50583436d70f
annie <annie_marie@estudante.sesisenai.org.br>
2026-04-13 11:47:23 -0300

    add - 6

--- javascripts/javacript.js
+++ javascripts/javacript.js
@@ -2,6 +2,7 @@ document.getElementById("sangueDoa").addEventListener("submit", function (e) {
     e.preventDefault();
 
     let nome = document.getElementById("nome").value.trim();
+    let sobrenome = document.getElementById("nome").value.trim();
     let email = document.getElementById("email").value.trim();
     let idade = document.getElementById("idade").value;
     let peso = document.getElementById("peso").value;
@@ -11,5 +12,4 @@ document.getElementById("sangueDoa").addEventListener("submit", function (e) {
     let estado = document.getElementById("estado").value.trim();
     let resultado = document.getElementById("resultado");
 
-   if ()
-
+   if 
