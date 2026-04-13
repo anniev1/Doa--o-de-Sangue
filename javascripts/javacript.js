93a24a131c063f5d8c06fba1609fb0cee304a010
annie <annie_marie@estudante.sesisenai.org.br>
2026-04-13 11:37:10 -0300

    add - 5

--- javascripts/javacript.js
+++ javascripts/javacript.js
@@ -1,19 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("sangueDoa").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    let form = document.getElementById("sangueDoa");
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let tipo = document.getElementById("tipo").value;
+    let telefone = document.getElementById("telefone").value.trim();
+    let cidade = document.getElementById("cidade").value.trim();
+    let estado = document.getElementById("estado").value.trim();
+    let resultado = document.getElementById("resultado");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
+   if ()
 
-        let nome = document.getElementById("nome").value.trim();
-        let email = document.getElementById("email").value.trim();
-        let idade = document.getElementById("idade").value;
-        let peso = document.getElementById("peso").value;
-        let tipo = document.getElementById("tipo").value;
-        let telefone = document.getElementById("telefone").value.trim();
-        let cidade = document.getElementById("cidade").value.trim();
-        let estado = document.getElementById("estado").value.trim();
-
-        let resultado = document.getElementById("resultado");
-
-        
